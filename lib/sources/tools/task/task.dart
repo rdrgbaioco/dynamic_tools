da192cec35101fb1f207da7e3721783a5a81a15d
Samuel Rodrigo Baioco <rdrgbaioco@gmail.com>
2023-02-19 23:58:01 -0400

    Update task.dart

--- lib/sources/tools/task/task.dart
+++ lib/sources/tools/task/task.dart
@@ -1,6 +1,6 @@
 import 'package:dynamic_tools/dynamic_tools.dart';
 
-/// `AsyncTask<S, E>` represents an asynchronous computation.
+/// `Task<S, E>` represents an asynchronous computation.
 typedef Task<S extends Object, E extends Object> = Future<TaskObject<S, E>>;
 
 /// `AsyncResult<S, E>` represents an asynchronous computation.
